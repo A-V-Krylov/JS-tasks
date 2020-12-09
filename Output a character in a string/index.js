07635e2ded8b3b58a0d26c3d5f76f9d65e4bdc4e
Alexander Krylov <krylov.exceedteam@gmail.com>
2020-12-09 17:00:04 +0300

    Output a character in a string

---
+++ Output a character in a string/index.js
@@ -0,0 +1,8 @@
+/*Дана строка:
+`Я иучаю JavaScript`
+Как вывести 3-й символ? 1-й? Последний?*/
+
+let str = `Я изучаю JavaScript`;
+console.log(str[3]); // з
+console.log(str.charAt(0)); // Я
+console.log(str[str.length - 1] ); //t
